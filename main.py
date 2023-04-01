f01e4873252a135b52b4d93451a6f67971dc0221
abedul59 <70437442+abedul59@users.noreply.github.com>
2023-04-01 13:22:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-import openai
-
-import httpx
 from fastapi import FastAPI, Request
+from pydantic import BaseModel
+import httpx, os, openai
+app = FastAPI()
 
 
 TOKEN = str(os.getenv("TELEGRAM_BOT_TOKEN"))
@@ -10,7 +9,7 @@ BASE_URL = f"https://api.telegram.org/bot{TOKEN}"
 
 client = httpx.AsyncClient()
 
-app = FastAPI()
+
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 conversation = []
@@ -45,14 +44,42 @@ class ChatGPT:
         return response['choices'][0]['message']['content'].strip()
 
 chatgpt = ChatGPT()
-  
-    
-@app.post("/callback/")
+
+
+
+
+
+
+
+class Msg(BaseModel):
+    msg: str
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World. Welcome to FastAPI!"}
+
+
+@app.get("/path")
+async def demo_get():
+    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
+
+
+@app.post("/path")
+async def demo_post(inp: Msg):
+    return {"message": inp.msg.upper()}
+
+
+@app.get("/path/{path_id}")
+async def demo_get_path_id(path_id: int):
+    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+
+@app.post("/callback")
 async def webhook(req: Request):
     data = await req.json()
     chat_id = data['message']['chat']['id']
     text = data['message']['text']
-    ai_reply_response = chatgpt.get_response(text)  
+    ai_reply_response = chatgpt.get_response(text)
 
     await client.get(f"{BASE_URL}/sendMessage?chat_id={chat_id}&text={ai_reply_response}")
 
